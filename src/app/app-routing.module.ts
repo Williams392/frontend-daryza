8a98bbb0dd834742be1b305f1d636a932dd8250c
Williams392 <williamsvalle392@gmail.com>
2024-09-28 02:57:26 -0500

    login y core

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  
   { path: '', redirectTo: 'admin', pathMatch: 'full' },
-  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
+
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+
+  { path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+
 ];
 
 @NgModule({
